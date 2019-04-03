d6a884c0084a90a2fe282979f1716151df00abd7
eltsssu <elli.savola@gmail.com>
2019-04-03 15:12:17 +0300

    Ehdokkaan lisäys servletti luotu

---
+++ vaalikones/src/main/java/vaalikone/EhdokkaanLisays.java
@@ -0,0 +1,40 @@
+package vaalikone;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class EhdokkaanLisays
+ */
+
+public class EhdokkaanLisays extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public EhdokkaanLisays() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("TÄÄLLÄ LISÄTÄÄN UUSI EHDOKAS! ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
